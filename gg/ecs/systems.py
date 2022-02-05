a3df2b65284d7622e45b9df80b5260d3f33c8931
Willie Johnson <liwa.johnson@gmail.com>
2022-02-05 12:16:07 -0500

    implement: basic systems class

--- gg/ecs/systems.py
+++ gg/ecs/systems.py
@@ -1,17 +1,26 @@
 import pygame
 
 from dataclasses import dataclass
+from typing import List
 
 from . import objects
+
+
+
 
 @dataclass
 class System:
     name: str
-    objects = pygame.sprite.Group()
+    objects: List[objects.Object]
     
     def update(self) -> None:
+        if objects is None: return
+        
         for object in self.objects:
             object.update(self)
     
 Weapons = System
-Entities = System
+Bodies = System
+Decay = System
+Stats = System
+
